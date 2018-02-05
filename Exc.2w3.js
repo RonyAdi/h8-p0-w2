4e1969a53f355769f7b7ffce7ee03113c2b2f05a
Rejeki_Agung <Rejeki_Agung@ILoopss-MacBook-Air.local>
2018-02-05 17:23:42 +0700

    Tugas week 3

---
+++ Exc.2w3.js
@@ -0,0 +1,16 @@
+// Buatlah sebuah fungsi dengan nama balikString. Fungsi ini akan menerima argumen sebuah string dan mengembalikan kebalikannya.
+
+// Example
+// //input "hello world!"
+// //output
+// "!dlrow olleh"
+
+function balikString() {
+    var arr = [];
+    var kata = 'hello world'
+    for (var i = kata.length-1; i >= 0; i--){
+        arr = arr + kata[i]
+    }
+    return arr;
+}
+console.log(balikString());
